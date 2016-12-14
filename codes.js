e09ecd49e5c266f33db64904fefec5a4422753b4
Andrew George <DevAndrewGeorge@gmail.com>
2016-12-13 20:56:51 -0800

    Add setProperty() method to hash.js

---
+++ codes.js
@@ -0,0 +1,7 @@
+module.exports = {
+   NONE: 0,
+   USER_EXISTS: 1,
+   USER_DNE: 2,
+   PASSWORD_INVALID: 3,
+   ILLEGAL_CREDENTIALS: 4
+}
--- hash.js
+++ hash.js
@@ -8,7 +8,27 @@ const crypto = require('crypto');
 //I dislike arbitrary settings here. These settings need to be accessible (and changeable) elsewhere
 settings = {
    iterations: 20000,
-   hashLength: 64
+   length: 64
+}
+
+
+
+
+
+//
+function setProperty(property, value) {
+   switch(property[0]) {
+      case 'iterations':
+      case 'length':
+         if (typeof value !== "number")  throw new TypeError('sl.hash.setProperty: desired sl.hash."' + property[0] +'" value is not of required type number.');
+         if (value < 1) throw new RangeError("sl.hash.setProperty: " + property[0] + "(" + value + ") is not positive.");
+         if (Math.round(value) !== value) throw new RangeError("sl.hash.setProperty: " + property[0] + "(" + value + ") is not an interger value.");
+         settings[property[0]] = value;
+         break;
+      default:
+         throw new ReferenceError('sl.hash.setProperty: "' + property[0] + '" is not a sl.hash property. You cannot set its value.');
+         break;
+   }
 }
 
 
@@ -19,7 +39,7 @@ settings = {
 function hash(credentials, callback) {
    //generating salt if necessary
    if (!credentials.has("$salt")) {
-      crypto.randomBytes(settings.hashLength, (err, salt) => {
+      crypto.randomBytes(settings.length, (err, salt) => {
          if (err) { callback(err); return; }
          credentials.set("$salt", salt.toString("hex"));
          hash(credentials, callback);
@@ -28,7 +48,7 @@ function hash(credentials, callback) {
    }
 
    //hasing
-   crypto.pbkdf2(credentials.get("$password"), credentials.get("$salt"), settings.iterations, settings.hashLength, "sha256", (err, hash) => {
+   crypto.pbkdf2(credentials.get("$password"), credentials.get("$salt"), settings.iterations, settings.length, "sha256", (err, hash) => {
       if (err) { callback(err); return; }
       credentials.set("$hash", hash.toString("hex"));
       credentials.set("$iterations", settings.iterations);
@@ -41,6 +61,7 @@ function hash(credentials, callback) {
 
 
 module.exports = hash;
+module.exports.setProperty = setProperty;
 
 //for testing
 module.exports.hash = hash;
